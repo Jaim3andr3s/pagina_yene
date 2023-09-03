27529e7aeb832304fde6dff074ab9662707bb41b
Jaime Gil <jgilamaris49@gmail.com>
2023-09-03 16:21:37 -0500

    saico

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function agregarLibrosAlPerfil(perfil) {
     if (perfil === 'perfil1') {
         const librosPerfil1 = [
             {
-                titulo: 'EL HIPO DEL TUCAN',
+                titulo: 'El hipo del tucan',
                 pdfUrl: 'https://drive.google.com/file/d/1cNmpawdm3rLUtuHxxHfMG64zARposf1G/view?usp=drive_link', // Reemplaza esto con la URL correcta
                 portadaUrl: 'https://mobbyt.com/media/gallery/44f11ea12f297cf0a9acfce336dca2f1/12c974f2_2021-06-07.png'
             },
@@ -37,42 +37,42 @@ function agregarLibrosAlPerfil(perfil) {
             const li = document.createElement('li');
             const a = document.createElement('a');
             
-            // Contenedor principal para el libro (imagen y texto)
-            const container = document.createElement('div');
-            container.classList.add('book-container');
-            
             // Agregar la imagen de la portada como enlace al PDF
             const img = document.createElement('img');
             img.src = libro.portadaUrl;
-            a.href = libro.pdfUrl;
-            a.target = '_blank'; // Abre el enlace en una nueva ventana/tabla
-            
-            // Contenedor para el texto del libro
-            const textContainer = document.createElement('div');
-            textContainer.classList.add('book-text-container');
-            
-            const titulo = document.createElement('p');
-            titulo.textContent = libro.titulo;
+            a.appendChild(img);
             
             // Configurar el evento mouseover para mostrar el texto
-            container.addEventListener('mouseover', () => {
-                textContainer.style.opacity = '1'; // Muestra el texto al pasar el mouse
+            img.classList.add('book-cover'); // Agrega la clase book-cover
+            img.addEventListener('mouseover', () => {
+                // Crear un elemento de div para contener el título del libro
+                const titleContainer = document.createElement('div');
+                titleContainer.classList.add('title-container');
+        
+                // Agregar el título del libro como texto
+                const title = document.createElement('p');
+                title.textContent = libro.titulo;
+                title.classList.add('book-text');
+                titleContainer.appendChild(title);
+        
+                // Agregar el elemento de texto al li
+                li.appendChild(titleContainer);
             });
             
             // Configurar el evento mouseout para ocultar el texto
-            container.addEventListener('mouseout', () => {
-                textContainer.style.opacity = '0'; // Oculta el texto al quitar el mouse
+            img.addEventListener('mouseout', () => {
+                const titleContainer = li.querySelector('.title-container');
+                if (titleContainer) {
+                    li.removeChild(titleContainer); // Elimina el elemento de texto al quitar el mouse
+                }
             });
             
-            textContainer.appendChild(titulo);
-            container.appendChild(img);
-            container.appendChild(textContainer);
-            a.appendChild(container);
+            a.href = libro.pdfUrl;
+            a.target = '_blank'; // Abre el enlace en una nueva ventana/tabla
             
             li.appendChild(a);
             bookList.appendChild(li);
         });
-        
         
         
     } else if (perfil === 'perfil2') {
