5d3ed65c5deba600c9ca71cc3f66e306f4088ddf
Jaime Gil <jgilamaris49@gmail.com>
2023-09-03 14:22:46 -0500

    primer libro

--- script.js
+++ script.js
@@ -7,19 +7,67 @@ function elegirPerfil(perfil) {
     const librarySection = document.getElementById('librarySection');
     librarySection.style.display = 'block';
 
-    // Agregar libros de ejemplo a la biblioteca (puedes personalizar esta parte)
+    // Llamar a la función para agregar libros al perfil seleccionado
+    agregarLibrosAlPerfil(perfil);
+}
+
+function agregarLibrosAlPerfil(perfil) {
+    // Limpiar la lista de libros actual
     const bookList = document.getElementById('bookList');
+    bookList.innerHTML = '';
+
+    // Agregar libros según el perfil seleccionado
     if (perfil === 'perfil1') {
-        bookList.innerHTML = `
-            <li>Libro 1</li>
-            <li>Libro 2</li>
-            <li>Libro 3</li>
-        `;
+        const librosPerfil1 = [
+            {
+                titulo: 'El hipo del tucán',
+                pdfUrl: 'https://drive.google.com/file/d/1cNmpawdm3rLUtuHxxHfMG64zARposf1G/view?usp=drive_link' // Reemplaza esto con la URL correcta
+            },
+            {
+                titulo: 'Libro 2',
+                pdfUrl: 'https://example.com/ruta_al_archivo_pdf_del_libro2.pdf' // Reemplaza esto con la URL correcta
+            },
+            {
+                titulo: 'Libro 3',
+                pdfUrl: 'https://example.com/ruta_al_archivo_pdf_del_libro3.pdf' // Reemplaza esto con la URL correcta
+            }
+        ];
+
+        librosPerfil1.forEach(libro => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.textContent = libro.titulo;
+            a.href = libro.pdfUrl;
+            a.target = '_blank'; // Abre el enlace en una nueva ventana/tabla
+            li.appendChild(a);
+            bookList.appendChild(li);
+        });
+
     } else if (perfil === 'perfil2') {
-        bookList.innerHTML = `
-            <li>Libro 4</li>
-            <li>Libro 5</li>
-            <li>Libro 6</li>
-        `;
+        const librosPerfil2 = [
+            {
+                titulo: 'Libro 4',
+                pdfUrl: 'ruta_al_archivo_pdf_del_libro4.pdf'
+            },
+            {
+                titulo: 'Libro 5',
+                pdfUrl: 'ruta_al_archivo_pdf_del_libro5.pdf'
+            },
+            {
+                titulo: 'Libro 6',
+                pdfUrl: 'ruta_al_archivo_pdf_del_libro6.pdf'
+            }
+        ];
+
+        librosPerfil2.forEach(libro => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.textContent = libro.titulo;
+            a.href = libro.pdfUrl;
+            a.target = '_blank'; // Abre el enlace en una nueva ventana/tabla
+            li.appendChild(a);
+            bookList.appendChild(li);
+        });
     }
+    // ... (otros perfiles)
 }
